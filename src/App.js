6d6aa0e86de89a2f79ee87299b137390f8650c88
Ronak Lala <ronaklala2010@gmail.com>
2023-03-26 21:51:38 +0530

    Completed the test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://insurance-api-five.vercel.app/agents/get")
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading === true ? (
+        <>
+          <h1>Loading Data...</h1>
+        </>
+      ) : (
+        <>
+          <table id="customers">
+            <tr>
+              <th>Name</th>
+              <th>City</th>
+              <th>Insurnace Category</th>
+              <th>Insurance Type</th>
+            </tr>
+            {data.map((d, i) => (
+              <>
+                <tr key={i}>
+                  <td>{d.name}</td>
+                  <td>{d.city}</td>
+                  <td>{d.category}</td>
+                  <td>{d.type}</td>
+                </tr>
+              </>
+            ))}
+          </table>
+        </>
+      )}
     </div>
   );
 }
